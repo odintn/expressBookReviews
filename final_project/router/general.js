b0cc018993bb4ee675c536bd442bd4ccc0bb13b0
odintn <nodintsova@optonline.net>
2024-10-10 23:32:59 -0400

    implemented CRUD operations asynchronously

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,39 +39,70 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books,null,4));
+    const get_books = new Promise((resolve, reject) => {
+        res.send(JSON.stringify(books, null, 4));
+        resolve("GET books request completed");
+    });
+
+    get_books.then(
+        (message) => console.log(message)
+    );
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
- });
+    const get_books = new Promise((resolve, reject) => {
+        const isbn = req.params.isbn;
+        if(books[isbn]){
+            res.send(books[isbn]);
+        }else{
+            res.status(404).send({"message": "Book not found"});
+        }
+        resolve("GET by ISBN request completed");
+    });
+
+    get_books.then((message) => console.log(message));
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  let booksByAuthor = [];
-  Object.keys(books).forEach(key => {
-    let book = books[key];
-    if(book.author === author){
-        booksByAuthor.push(book);
-    }
-  });
-  res.send(JSON.stringify(booksByAuthor));
+    const get_books = new Promise((resolve, reject) => {
+        const author = req.params.author;
+        let booksByAuthor = [];
+        Object.keys(books).forEach(key => {
+            let book = books[key];
+            if(book.author === author){
+                booksByAuthor.push(book);
+            }
+        });
+        res.send(JSON.stringify(booksByAuthor));
+        resolve("GET by Author request completed");
+    });
+
+    get_books.then(
+        (message) => console.log(message)
+    );
+  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    let booksByTitle = [];
-    Object.keys(books).forEach(key => {
-      let book = books[key];
-      if(book.title === title){
-          booksByTitle.push(book);
-      }
+    const get_books = new Promise((resolve, reject) => {
+        const title = req.params.title;
+        let booksByTitle = [];
+        Object.keys(books).forEach(key => {
+            let book = books[key];
+            if(book.title === title){
+                booksByTitle.push(book);
+            }
+        });
+        res.send(JSON.stringify(booksByTitle));
+        resolve("GET by Title request completed.");
     });
-    res.send(JSON.stringify(booksByTitle));
+
+    get_books.then(
+        (message) => console.log(message)
+    );
 });
 
 //  Get book review
